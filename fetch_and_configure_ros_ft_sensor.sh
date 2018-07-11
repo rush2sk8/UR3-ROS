ac3308d93b9d1efcdf1a3e4fd85cec8d60df189c
rushadantia <rushadantia@gmail.com>
2018-07-11 14:38:06 -0400

    updated fetch to delete .git and changed color

--- fetch_and_configure_ros_ft_sensor.sh
+++ fetch_and_configure_ros_ft_sensor.sh
@@ -14,12 +14,12 @@ catkin_make
 source devel/setup.bash
 
 #modify the demo.launch file
-echo "$(tput setaf 1)Enter the FT Sensor's IP Address:$(tput sgr0)"
+echo "$(tput setaf 2)Enter the FT Sensor's IP Address:$(tput sgr0)"
 read -p "" ip_addr
 sed -i -e "s/192.168.100.12/$ip_addr/g" ./src/etherdaq_ros/launch/demo.launch
 
 #generate a run file
 echo "source devel/setup.bash; roslaunch optoforce_etherdaq_driver  demo.launch" > run.sh
 chmod +x run.sh
-
+rm -rf ./src/etherdaq_ros/.git
 
