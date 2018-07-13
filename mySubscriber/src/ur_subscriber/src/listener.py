9a64185cf58694d5eb0c099f3006c9c84b745807
Rushad Antia <rushadantia@gmail.com>
2018-07-13 11:19:57 -0400

    Update listener.py

--- mySubscriber/src/ur_subscriber/src/listener.py
+++ mySubscriber/src/ur_subscriber/src/listener.py
@@ -4,7 +4,7 @@ from geometry_msgs.msg import WrenchStamped
 
 
 def callback(msg):
-	rospy.loginfo(rospy.get_caller_id() + "%u Fx:%.2f Fy:%.2f Fz:%.2f Tx:%.2f Ty:%.2f Tz:%.2f \r\n", msg.header.seq, msg.wrench.force.x, msg.wrench.force.y, msg.wrench.force.z, msg.wrench.torque.x, msg.wrench.torque.y, msg.wrench.torque.z)
+	rospy.loginfo("%u Fx:%.2f Fy:%.2f Fz:%.2f Tx:%.2f Ty:%.2f Tz:%.2f \r\n", msg.header.seq, msg.wrench.force.x, msg.wrench.force.y, msg.wrench.force.z, msg.wrench.torque.x, msg.wrench.torque.y, msg.wrench.torque.z)
 
 def listener():
 	rospy.init_node('listener', anonymous=True)
